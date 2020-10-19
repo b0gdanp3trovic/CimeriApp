ed0f5086db69644103a947eb110a4db119c6171a
bp3201 <bp3201@student.uni-lj.si>
2020-10-19 16:56:45 +0200

    Ipak firebase

--- app/src/main/java/com/example/cimeriapp/LoginActivity.kt
+++ app/src/main/java/com/example/cimeriapp/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.cimeriapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -10,9 +11,13 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     val RC_SIGN_IN = 1
     lateinit var mGoogleSignInClient: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +38,10 @@ class LoginActivity : AppCompatActivity() {
             googleSignIn()
         }
 
+        loginButton.setOnClickListener {
+            emailPassSignIn()
+        }
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -40,19 +49,31 @@ class LoginActivity : AppCompatActivity() {
 
         if (requestCode == RC_SIGN_IN) {
             val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
-            handleSignInResult(task)
+            handleGoogleSignInResult(task)
         }
     }
 
 
-
+    fun emailPassSignIn() {
+        auth = Firebase.auth
+        auth.signInWithEmailAndPassword(loginEmail.text.toString(), loginPassword.text.toString()).addOnCompleteListener(this) { task ->
+            if(task.isSuccessful) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                Log.w("Registration", "Registration error!")
+                Toast.makeText(baseContext, "Authentication failed", Toast.LENGTH_SHORT).show()
+                Log.w("Reg", task.exception.toString())
+            }
+        }
+    }
 
     fun googleSignIn() {
         val intent: Intent = mGoogleSignInClient.signInIntent
         startActivityForResult(intent, RC_SIGN_IN)
     }
 
-    fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+    fun handleGoogleSignInResult(completedTask: Task<GoogleSignInAccount>) {
         try {
             val account: GoogleSignInAccount? = completedTask.getResult(ApiException::class.java)
             Toast.makeText(this, "Successfully signed in as ${account?.displayName}.", Toast.LENGTH_LONG).show()
--- app/src/main/java/com/example/cimeriapp/RegisterActivity.kt
+++ app/src/main/java/com/example/cimeriapp/RegisterActivity.kt
@@ -1,30 +1,48 @@
 package com.example.cimeriapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.parse.ParseUser
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        auth = Firebase.auth
 
         regButton.setOnClickListener {
-            register()
+            registerViaEmail()
         }
     }
 
 
-    fun register() {
+    fun registerViaEmail() {
         if(dataValid()) {
-            val user: ParseUser = ParseUser()
+            auth.createUserWithEmailAndPassword(regEmail.text.toString(), regPassword.text.toString()).addOnCompleteListener(this) {task ->
+                if(task.isSuccessful) {
+                    Log.i("Registration", "Registration successful!")
+                    val user = auth.currentUser
+                } else {
+                    Log.w("Registration", "Registration error!")
+                    Toast.makeText(baseContext, "Authentication failed", Toast.LENGTH_SHORT).show()
+                    Log.w("Reg", task.exception.toString())
+                    val intent: Intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                }
+            }
         }
     }
 
     fun dataValid(): Boolean  {
         // TODO Provera validnosti unetih podataka
-
         return true
     }
 }
