142ce76810857362362c8b8fd69ba1a15496024b
bp3201 <bp3201@student.uni-lj.si>
2020-10-24 02:13:34 +0200

    Reg data checking

--- app/src/main/java/com/example/cimeriapp/RegisterActivity.kt
+++ app/src/main/java/com/example/cimeriapp/RegisterActivity.kt
@@ -1,13 +1,15 @@
 package com.example.cimeriapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
@@ -26,23 +28,59 @@ class RegisterActivity : AppCompatActivity() {
 
     fun registerViaEmail() {
         if(dataValid()) {
-            auth.createUserWithEmailAndPassword(regEmail.text.toString(), regPassword.text.toString()).addOnCompleteListener(this) {task ->
+            auth.createUserWithEmailAndPassword(
+                regEmail.text.toString(),
+                regPassword.text.toString()
+            ).addOnCompleteListener(this) { task ->
                 if(task.isSuccessful) {
                     Log.i("Registration", "Registration successful!")
                     val user = auth.currentUser
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
                 } else {
                     Log.w("Registration", "Registration error!")
-                    Toast.makeText(baseContext, "Authentication failed", Toast.LENGTH_SHORT).show()
                     Log.w("Reg", task.exception.toString())
-                    val intent: Intent = Intent(this, LoginActivity::class.java)
-                    startActivity(intent)
+                    // DA L POSTOJI U BAZI VEĆ
+                    try {
+                        throw task.exception as Throwable
+                    }
+                    catch(e: FirebaseAuthUserCollisionException) {
+                        regEmail.error = getString(R.string.collisionEmail)
+                        regEmail.requestFocus()
+                    }
                 }
             }
         }
     }
 
     fun dataValid(): Boolean  {
-        // TODO Provera validnosti unetih podataka
+        //IME I PREZIME
+        if(regFullName.text.toString().isEmpty()){
+            regFullName.error = getString(R.string.invalidFullName)
+            regFullName.requestFocus()
+            return false
+        }
+        //EMAIL
+        if(!android.util.Patterns.EMAIL_ADDRESS.matcher(regEmail.text.toString()).matches()) {
+            regEmail.error = getString(R.string.invalidEmail)
+            regEmail.requestFocus()
+            return false
+        }
+
+        //PASS
+        if(regPassword.text.toString().length < 8 || regPassword.text.toString().count { c -> c.isUpperCase() } == 0) {
+            regPassword.error = (getString(R.string.passwordReq))
+            regPassword.requestFocus()
+            return false
+        }
+        //PASS POTVRDA
+        if(!regPassword.text.toString().equals(regConfirmPassword.text.toString())) {
+            regPassword.error = getString(R.string.passwordMatch)
+            regPassword.requestFocus()
+            return false
+        }
         return true
     }
+
+
 }
