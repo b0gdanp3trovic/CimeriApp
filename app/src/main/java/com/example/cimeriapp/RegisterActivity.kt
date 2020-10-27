c7ed3804609f9d9267638c7542bb4e49abe7d27c
Никола Кркић <49657272+Krkula2Krka@users.noreply.github.com>
2020-10-27 01:23:00 +0100

    2

--- app/src/main/java/com/example/cimeriapp/RegisterActivity.kt
+++ app/src/main/java/com/example/cimeriapp/RegisterActivity.kt
@@ -3,6 +3,9 @@ package com.example.cimeriapp
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
