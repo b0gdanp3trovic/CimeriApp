ed20f155964d5207e4b3d76c99467267b6dc3fec
bp3201 <bp3201@student.uni-lj.si>
2020-11-01 08:23:09 +0100

    Pocetak homepage liste (ne znam da l valja ovako, ce vidimo)

--- app/src/main/java/com/example/cimeriapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/cimeriapp/activities/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.cimeriapp.activities
 
+import android.app.SearchManager
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.cimeriapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (Intent.ACTION_SEARCH == intent.action) {
+            intent.getStringExtra(SearchManager.QUERY)?.also { query ->
+                doSearch(query)
+            }
+        }
+    }
+
+    private fun doSearch(query: String) {
+        Log.i("query", query)
     }
 }
