6a9fec7b7b35988128b9e77219ef72eb7792eb74
Никола Кркић <49657272+Krkula2Krka@users.noreply.github.com>
2020-11-11 23:24:20 +0100

    Add files via upload

---
+++ app/src/main/java/com/example/cimeriapp/fragments/ContactsFragment.kt
@@ -0,0 +1,32 @@
+package com.example.cimeriapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cimeriapp.R
+import com.example.cimeriapp.additions.MyAdapter
+import com.example.cimeriapp.additions.StaticObjects
+
+class ContactsFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view= inflater.inflate(R.layout.contacts_fragment, container, false)
+        recyclerView = view.findViewById<RecyclerView>(R.id.contacts_recycler_view).apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(context)
+            adapter = MyAdapter(StaticObjects.myData)
+//            Toast.makeText(context, StaticObjects.myData.toString(), Toast.LENGTH_LONG).show()
+        }
+        return view
+    }
+}
