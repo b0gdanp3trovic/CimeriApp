2a5d97c32acf1da0d50a7dac07cb984c7c7901c2
Никола Кркић <49657272+Krkula2Krka@users.noreply.github.com>
2020-11-11 23:44:27 +0100

    Update LoginActivity.kt

--- app/src/main/java/com/example/cimeriapp/activities/LoginActivity.kt
+++ app/src/main/java/com/example/cimeriapp/activities/LoginActivity.kt
@@ -19,7 +19,6 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
-import MainActivity
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
